a2ef87ab3fc10f2e02c9348117ecf54dc47a6092
Neil Creveling <neil.creveling@gmail.com>
2021-02-27 16:35:54 -0500

    global variables added to js file

---
+++ script.js
@@ -0,0 +1,8 @@
+// create global variables
+var timeLeft; // time left
+var timeInterval; // timer interval
+var quizCurrent; // current question
+var answerChoice; // the 4 answer choices
+var finalScore; // score at the end of the game
+var currentLeaderboard; // current leaderboard
+var initials; // name entry value
